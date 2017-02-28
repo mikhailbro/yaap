de99dd059bf4ae3baf1d0f9b5f12a91aeb4ef689
PZ <pz@PZs-MacBook-Pro.local>
2017-02-28 21:50:56 +0100

    various changes in models and model logic

--- yaap/common/models/api.js
+++ yaap/common/models/api.js
@@ -13,8 +13,16 @@ module.exports = function(Api) {
 	Api.disableRemoteMethodByName('updateAll');					// POST		/resource/update
 	Api.disableRemoteMethodByName('upsertWithWhere');			// POST		/resource/upsertWithWhere
 
+	// Disable some relational functions 
+	Api.disableRemoteMethodByName('prototype.__create__clients');		// POST		/resource/:id/clients
+	Api.disableRemoteMethodByName('prototype.__findById__clients');		// GET		/resource/:id/clients/:clientId
+	Api.disableRemoteMethodByName('prototype.__updateById__clients');	// PUT		/resource/:id/clients/:clientId
+	Api.disableRemoteMethodByName('prototype.__destroyById__clients');	// DELETE	/resource/:id/clients/:clientId
+	Api.disableRemoteMethodByName('prototype.__count__clients');		// GET 		/resource/:id/clients/count
+	Api.disableRemoteMethodByName('prototype.__link__clients');
+	Api.disableRemoteMethodByName('prototype.__unlink__clients');
 
-	// GET all apis
+	// GET /apis
 	Api.beforeRemote('find', function(context, unused, next) {
 	    if (!context.args.filter || !context.args.filter.where) {
 	    	// No 'where' filter in request, add where clause to check audience
@@ -26,7 +34,23 @@ module.exports = function(Api) {
 	    next();
 	 });
 
-	// GET one api by id - check response as no where clause can be set in beforeRemote()
+	// POST /apis
+	Api.beforeRemote('create', function(context, unused, next) {
+		// Check that tenantId from body matches one tenant from apiOwner Role from token
+		if (isTenantOk(context.req.body.tenantId, context.req.apiOwnerTenants)) {
+			next();
+		} else {
+			var error = new Error();
+			error.status = 400;
+			error.message = 'Wrong tenantId in request body.';
+			error.code = 'BAD_REQUEST';
+			error.stack = "";
+			next(error);
+		}
+	});
+
+	// GET /apis/{id}
+	//check response as no where clause can be set in beforeRemote()
 	Api.afterRemote('findById', function(context, response, next) {
 		if (!response) {
 			next();
@@ -46,22 +70,30 @@ module.exports = function(Api) {
 		}
 	 });
 
-	// POST new api
-	Api.beforeRemote('create', function(context, unused, next) {
-		// Check that tenantId from body matches one tenant from apiOwner Role from token
-		if (isTenantOk(context.req.body.tenantId, context.req.apiOwnerTenants)) {
-			next();
-		} else {
-			var error = new Error();
-			error.status = 400;
-			error.message = 'Wrong tenantId in request body.';
-			error.code = 'BAD_REQUEST';
-			error.stack = "";
-			next(error);
-		}
+	// PUT /apis/{id} and  POST /apis/{id}/replace
+	Api.beforeRemote('replaceById', function(context, unused, next) {
+		Api.findById(context.req.params.id, { fields: {tenantId: true} }, function(err, api) {
+			if (err) {
+				console.log("ERROR");
+				console.log(err);
+				next(err);
+			}
+				
+			if (isTenantOk(api.tenantId, context.req.apiOwnerTenants)) {
+				next();
+			} else {
+				var error = new Error();
+				error.status = 404;
+				error.message = 'Unknown "api" id "' + context.req.params.id + '".';
+				error.code = 'MODEL_NOT_FOUND';
+				error.stack = "";
+				next(error);
+			}
+
+		});
 	});
 
-	// DELETE api
+	// DELETE /apis/{id}
 	Api.beforeRemote('deleteById', function(context, unused, next){
 		Api.findById(context.req.params.id, { fields: {tenantId: true} }, function(err, api) {
 			if (err) {
@@ -84,27 +116,17 @@ module.exports = function(Api) {
 		});
 	});
 
-	// PUT api (and POST /replace)
-	Api.beforeRemote('replaceById', function(context, unused, next) {
-		Api.findById(context.req.params.id, { fields: {tenantId: true} }, function(err, api) {
-			if (err) {
-				console.log("ERROR");
-				console.log(err);
-				next(err);
-			}
-				
-			if (isTenantOk(api.tenantId, context.req.apiOwnerTenants)) {
-				next();
-			} else {
-				var error = new Error();
-				error.status = 404;
-				error.message = 'Unknown "api" id "' + context.req.params.id + '".';
-				error.code = 'MODEL_NOT_FOUND';
-				error.stack = "";
-				next(error);
-			}
+	// GET /apis/{id}/clients
+	Api.afterRemote('prototype.__get__clients', function(context, response, next) {
+		// TODO!!
+		// maybe beforeRemote() instead....
+		next();
+	});
 
-		});
+	// DELETE /apis/{id}/clients
+	Api.beforeRemote('prototype.__delete__clients', function(context, response, next) {
+		// TODO!!
+		next();
 	});
 
 	function isTenantOk(tenantId, tenants) {
--- yaap/common/models/client.js
+++ yaap/common/models/client.js
@@ -2,4 +2,14 @@
 
 module.exports = function(Client) {
 
+	// Disable some functions via REST API according to http://loopback.io/doc/en/lb3/Exposing-models-over-REST.html
+	Client.disableRemoteMethodByName('exists'); 					// GET		/resource/:id/exists
+	Client.disableRemoteMethodByName('findOne');					// GET		/resource/findOne
+	Client.disableRemoteMethodByName('count');						// GET 		/resource/count
+	Client.disableRemoteMethodByName('createChangeStream');			// POST		/resource/change-stream
+	Client.disableRemoteMethodByName('patchOrCreate');				// PATCH	/resource
+	Client.disableRemoteMethodByName('replaceOrCreate');			// PUT		/resource
+	Client.disableRemoteMethodByName('prototype.patchAttributes');	// PATCH	/resource/:id
+	Client.disableRemoteMethodByName('updateAll');					// POST		/resource/update
+	Client.disableRemoteMethodByName('upsertWithWhere');			// POST		/resource/upsertWithWhere
 };
