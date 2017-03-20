bae2a9b3c8284042687c771a39e26b1beb0a5bdf
Mikhail <brodski@gmail.com>
2017-03-20 12:40:24 +0000

    client.js

--- yaap/common/models/client.js
+++ yaap/common/models/client.js
@@ -1,7 +1,8 @@
 'use strict';
 
 module.exports = function(Client) {
-
+	var uuid = require('node-uuid');
+	
 	// Disable some functions via REST API according to http://loopback.io/doc/en/lb3/Exposing-models-over-REST.html
 	Client.disableRemoteMethodByName('exists'); 					// GET		/clients/:id/exists
 	Client.disableRemoteMethodByName('findOne');					// GET		/clients/findOne
@@ -13,5 +14,119 @@ module.exports = function(Client) {
 	Client.disableRemoteMethodByName('updateAll');					// POST		/clients/update
 	Client.disableRemoteMethodByName('upsertWithWhere');			// POST		/clients/upsertWithWhere
 
+
+	// GET /clients
+	Client.beforeRemote('find', function(context, unused, next) {
+		if (!context.args.filter || !context.args.filter.where) {
+	    	// No 'where' filter in request, add where clause to check audience
+	    	context.args.filter = {where: { audience: { inq: context.req.apiConsumerTenants}}};
+	    } else {
+	    	// 'where' clause in request, add AND clause to check audience
+	    	context.args.filter.where = {and: [{audience: { inq: context.req.apiConsumerTenants}}, context.args.filter.where] };
+	    }
+	    next();
+	});
+	 
+	// GET /clients/{id}
+	//check response as no where clause can be set in beforeRemote()
+	Client.afterRemote('findById', function(context, response, next) {
+		if (!response) {
+			next();
+			return;
+		}
+		
+		// Check if user has role apiConsumer for the audience of the Client
+		if (checkAudience(response.audience, context.req.apiConsumerTenants)) {
+			console.log("ok");
+			next();
+		} else {
+			console.log("nok");
+			var error = new Error();
+			error.status = 404;
+			error.message = 'Unknown "client" id "' + response.id + '".';
+			error.code = 'MODEL_NOT_FOUND';
+			next(error);
+		}
+	});
+	 
+	// POST /clients
+	Client.beforeRemote('create', function(context, unused, next) {
+		// Check that tenantId from body matches one tenant from apiConsumer Role from token
+		if (isTenantInArray(context.req.body.tenantId, context.req.apiConsumerTenants)) {
+			var newId = uuid.v4();
+			context.req.body.id = newId;
+			next();
+		} else {
+			var error = new Error();
+			error.status = 400;
+			error.message = 'Wrong tenantId in request body.';
+			error.code = 'BAD_REQUEST';
+			next(error);
+		}
+	});
+	
+	// DELETE /clients/{id}
+	Client.beforeRemote('deleteById', function(context, unused, next){
+		Client.findById(context.req.params.id, { fields: {tenantId: true} }, function(err, client) {
+			if (err) {
+				console.log("ERROR");
+				console.log(err);
+				next(err);
+			}
+
+			if (client && isTenantInArray(client.tenantId, context.req.apiConsumerTenants)) {
+				next();
+			} else {
+				var error = new Error();
+				error.status = 404;
+				error.message = 'Unknown "client" id "' + context.req.params.id + '".';
+				error.code = 'MODEL_NOT_FOUND';
+				next(error);
+			}
+
+		});
+	});
+	
+	// PUT /clients/{id} and  POST /clients/{id}/replace
+	/*Client.beforeRemote('replaceById', function(context, unused, next) {
+		Client.findById(context.req.params.id, { fields: {tenantId: true} }, function(err, client) {
+			if (err) {
+				console.log("ERROR");
+				console.log(err);
+				next(err);
+			}
+				
+			if (isTenantInArray(api.tenantId, context.req.apiConsumerTenants)) {
+				next();
+			} else {
+				var error = new Error();
+				error.status = 404;
+				error.message = 'Unknown "client" id "' + context.req.params.id + '".';
+				error.code = 'MODEL_NOT_FOUND';
+				next(error);
+			}
+
+		});
+	});*/
+	
+	
+	function isTenantInArray(tenantId, tenants) {
+		// NOTE: array.indexOf() doesn't seem to work with mongodb generated ids....
+		var isOk = false;
+		for (var i = 0; i < tenants.length; i++) {
+			if (tenants[i] == tenantId) {
+				isOk = true;
+				break;
+			} 
+		}
+		return isOk;
+	}
+	
+	function checkAudience(audience, apiConsumers) {
+		// NOTE: array.indexOf() doesn't seem to work with mongodb generated ids....
+		return apiConsumers.some(function (v) {
+        	return audience.indexOf(v) >= 0;
+    	});
+	}
 	
 };
