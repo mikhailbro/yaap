d82253a8fbc5fed212ce89ddbee48d883795c01e
Unknown <pfust75@gmail.com>
2017-05-28 10:37:37 +0200

    Elastic Beanstalk config removed

--- yaap/server/server.js
+++ yaap/server/server.js
@@ -7,8 +7,8 @@ var app = module.exports = loopback();
 
 app.start = function() {
   // start the web server
-  var port = process.env.PORT || 8000;
-  return app.listen(port, function() {
+  var port = process.env.PORT || 3000;  // Custom code to use correct port on Heroku
+  return app.listen(port, function() { 
     app.emit('started');
     var baseUrl = app.get('url').replace(/\/$/, '');
     console.log('Web server listening at: %s', baseUrl);
